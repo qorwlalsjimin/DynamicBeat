c7c555176cbf9dbcf72e8671c56b4fc3b3a463f8
지민 백 <qorwlalsjimin@naver.com>
2022-08-26 09:30:34 +0900

    더블버퍼링으로 intro 이미지 불러오기

--- src/dynamic_beat_1/DynamicBeat.java
+++ src/dynamic_beat_1/DynamicBeat.java
@@ -1,8 +1,18 @@
 package dynamic_beat_1;
 
+import java.awt.Graphics;
+import java.awt.Image;
+
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 
 public class DynamicBeat extends JFrame{
+	
+	private Image screenImage;
+	private Graphics screenGraphic;
+	
+	private Image introBackground;
+	
 	public DynamicBeat() {
 		setTitle("Dynamic Beat");
 		setSize(Main.SCREEN_WIDTH, Main.SCREEN_HEIGHT);
@@ -10,5 +20,21 @@ public class DynamicBeat extends JFrame{
 		setLocationRelativeTo(null); //게임창이 모니터 정중앙에 뜨게 함
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //게임을 종료했을때 완벽히 종료
 		setVisible(true); //게임창을 화면에 보이게 함
+		
+		introBackground = new ImageIcon(Main.class.getResource("../images/introBackground.jpg")).getImage();
+	
+	}
+	
+	//화면을 그리는 메서드
+	public void paint(Graphics g) {
+		screenImage = createImage(Main.SCREEN_WIDTH, Main.SCREEN_HEIGHT);
+		screenGraphic = screenImage.getGraphics();
+		screenDraw(screenGraphic);
+		g.drawImage(screenImage, 0, 0, null);
+	}
+	
+	public void screenDraw(Graphics g) {
+		g.drawImage(introBackground, 0, 0, null);
+		this.repaint();
 	}
 }
---
+++ src/dynamic_beat_2/DynamicBeat.java
@@ -0,0 +1,14 @@
+package dynamic_beat_2;
+
+import javax.swing.JFrame;
+
+public class DynamicBeat extends JFrame{
+	public DynamicBeat() {
+		setTitle("Dynamic Beat");
+		setSize(Main.SCREEN_WIDTH, Main.SCREEN_HEIGHT);
+		setResizable(false);
+		setLocationRelativeTo(null); //게임창이 모니터 정중앙에 뜨게 함
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //게임을 종료했을때 완벽히 종료
+		setVisible(true); //게임창을 화면에 보이게 함
+	}
+}
